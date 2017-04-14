0150106191bff262804da04d00896be16e423379
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-04-13 21:03:49 -0300

    Updated home bg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,10 @@
 
         <title>Crazy Quiz</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #ebebeb;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
