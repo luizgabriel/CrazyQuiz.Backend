5b4eb9340ae79f0f9947a7600e65517457424392
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-04-13 21:08:43 -0300

    Updated random question query

--- app/Questionnaire.php
+++ app/Questionnaire.php
@@ -7,11 +7,12 @@ class Questionnaire implements IQuestionnaire
 {
     public function getRandomQuestion($level = 1, array $answeredQuestions = []): Question
     {
-        $question = Question::with('options')
-            ->where('level', $level)
-            ->whereNotIn('id', $answeredQuestions)
-            ->inRandomOrder()
-            ->first();
+        $question = Question::with('options')->where('level', $level);
+
+        if (count($answeredQuestions) > 0)
+            $question = $question->whereNotIn('id', $answeredQuestions);
+
+       $question = $question->inRandomOrder()->first();
 
         if (!$question && $this->hasExtraLevel($level))
             return $this->getRandomQuestion($level + 1, $answeredQuestions);
